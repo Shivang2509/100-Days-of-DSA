eeaa902568cdbe5a44ec27252b9ed89986edde6e
Shivang Gupta <iamshivang25@gmail.com>
2026-07-07 13:39:10 +0530

    Fifth Commit

---
+++ arrange_arrival_logs.py
@@ -0,0 +1,59 @@
+""" Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
+
+Input:
+- First line: integer p (number of entries in server log 1)
+- Second line: p sorted integers representing arrival times from server 1
+- Third line: integer q (number of entries in server log 2)
+- Fourth line: q sorted integers representing arrival times from server 2)
+
+Output:
+- Print a single line containing all arrival times in chronological order, separated by spaces
+
+Example:
+Input:
+5
+10 20 30 50 70
+4
+15 25 40 60
+
+Output:
+10 15 20 25 30 40 50 60 70
+
+Explanation: Compare the next unprocessed arrival time from both logs and append the earlier one to the final log until all entries are processed """
+
+
+
+
+
+
+""" CODE """
+
+n= int(input("enter the no of array: "))
+arr1= list(map(int, input("enter the array: ").split()))
+
+m= int(input("enter the no of array: "))
+arr2= list(map(int, input("enter the array: ").split()))
+
+i=0
+j=0
+result=[]
+
+while i<n and j<m:
+    if arr1[i]<arr2[j]:
+        result.append(arr1[i])
+        i += 1
+
+    else:
+        result.append(arr2[j])
+        j += 1
+
+while i<n:
+    result.append(arr1[i])
+    i += 1
+
+while j<m:
+    result.append(arr2[j])
+    j += 1
+
+print(*result)
+
